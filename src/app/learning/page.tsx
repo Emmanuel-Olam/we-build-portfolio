85a2e35ffd6c7a0405f0acdef1b4e89394c26f64
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-08-25 14:29:22 +0100

    feat: create learning page

---
+++ src/app/learning/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from 'react'
+import "../globals.css"
+import { Providers } from '../providers'
+import { Box } from '@chakra-ui/react'
+
+
+const Learning = () => {
+  return (
+    <Providers>
+      <Box>
+        <div className='text-[40px]'>
+          hlearning
+        </div>
+      </Box>
+    </Providers>
+  )
+}
+
+export default Learning
