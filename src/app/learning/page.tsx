a13ad3b4f6cf0663d26a255c753fadb8a4fb7498
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-08-26 11:37:58 +0100

    feat: complete first learning page

--- src/app/learning/page.tsx
+++ src/app/learning/page.tsx
@@ -4,14 +4,59 @@ import React from 'react'
 import "../globals.css"
 import { Providers } from '../providers'
 import { Box } from '@chakra-ui/react'
+import LearningFooter from '../components/learningFooter';
+import LearnFoot from '../components/learnFoot';
 
 
 const Learning = () => {
   return (
     <Providers>
       <Box>
-        <div className='text-[40px]'>
-          hlearning
+        <div id='#background' className='flex justify-center mt-[15px]'>
+          <div className=" w-[100%]">
+            <div className='lg:h-screen h-[466px] bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1692970506/learnhero_s2c0ye.svg")] shadow-[#000000] bg-cover bg-center w-full '>
+              <div className='lg:max-w-7xl mx-auto w-full '>
+                <div className='font-bold lg:text-[40px] text-[20px] text-shadow-[0_4px_16px_#000000] text-[#FFf] lg:w-[60%] w-[92%] lg:mx-[0px] mx-auto lg:pt-[25%] pt-[53%] '>
+                Discover a World of Opportunities With Freelancing Mastery.
+                </div>
+              </div>
+            </div>
+            <div className='lg:max-w-7xl w-[90%] mx-auto lg:flex lg:flex-col lg:justify-center mt-[15px]'>
+              <div className='text-[18px] lg:text-[34px] text-[#fff] font-bold lg:mx-auto'>Personal Information</div>
+              <form className='lg:w-[768px]'>
+                <div className=' mt-[19px] lg:mt-[30px] mb-[11px] lg:mb-[20px] '>
+                  <label htmlFor="first_name" className='text-[17px] lg:text-[24px] font-bold text-[#fff] lg:mx-auto'>FIRST NAME</label>
+                  <input className='lg:w-[372px] w-[337px] h-[33px] pl-[16px] rounded-[5px] border border-[#A09CB9] bg-[#0000] focus:ring-[#0000] mt-[8px] ' placeholder='Enter'/>
+                </div>
+                <div className=' mt-[19px] lg:mt-[30px] mb-[11px] lg:mb-[20px] '>
+                  <label htmlFor="other_name" className='text-[17px] lg:text-[24px] font-bold text-[#fff] lg:mx-auto'>OTHER NAMES</label>
+                  <input className='lg:w-[372px] w-[337px] h-[33px] pl-[16px] rounded-[5px] border border-[#A09CB9] bg-[#0000] focus:ring-[#0000] mt-[8px] ' placeholder='Enter'/>
+                </div>
+                <div className=' mt-[19px] lg:mt-[30px] mb-[11px] lg:mb-[20px] '>
+                  <label htmlFor="phone" className='text-[17px] lg:text-[24px] font-bold text-[#fff] lg:mx-auto'>PHONE NUMBER</label>
+                  <input className='lg:w-[372px] w-[337px] h-[33px] pl-[16px] rounded-[5px] border border-[#A09CB9] bg-[#0000] focus:ring-[#0000] mt-[8px] ' placeholder='Enter'/>
+                </div>
+                <div className=' mt-[19px] lg:mt-[30px] mb-[11px] lg:mb-[20px] '>
+                  <label htmlFor="email" className='text-[17px] lg:text-[24px] font-bold text-[#fff] lg:mx-auto'>EMAIL ADDRESS</label>
+                  <input className='lg:w-[372px] w-[337px] h-[33px] pl-[16px] rounded-[5px] border border-[#A09CB9] bg-[#0000] focus:ring-[#0000] mt-[8px] ' placeholder='Enter'/>
+                </div>
+                <div className=' mt-[19px] lg:mt-[30px] mb-[11px] lg:mb-[20px] '>
+                  <label htmlFor="nationality" className='text-[17px] lg:text-[24px] font-bold text-[#fff] lg:mx-auto'>NATIONALITY</label>
+                  <input className='lg:w-[372px] w-[337px] h-[33px] pl-[16px] rounded-[5px] border border-[#A09CB9] bg-[#0000] focus:ring-[#0000] mt-[8px] ' placeholder='Enter'/>
+                </div>
+                <div className=' mt-[19px] lg:mt-[30px] mb-[11px] lg:mb-[20px] '>
+                  <label htmlFor="state" className='text-[17px] lg:text-[24px] font-bold text-[#fff] lg:mx-auto'>STATE OF RESIDENCE</label>
+                  <input className='lg:w-[372px] w-[337px] h-[33px] pl-[16px] rounded-[5px] border border-[#A09CB9] bg-[#0000] focus:ring-[#0000] mt-[8px] ' placeholder='Enter'/>
+                </div>
+                <button className='bg-[#3E205C] lg:text-[] text-[#fff] text-[16px] font-medium mt-[18px] w-[196px] h-[46px] rounded-[10px] '>Save and Continue</button>
+              </form>
+              
+              <LearningFooter />
+            </div>
+            <LearnFoot />
+            
+          </div>
+
         </div>
       </Box>
     </Providers>
