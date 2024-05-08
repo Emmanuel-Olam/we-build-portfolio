83a3b2d6330d3696cb66e78b7a1973e952e41fec
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-05-08 21:58:49 +0100

    feat: create component for GIFdisplay

---
+++ src/app/components/LandingPage/GifDisplay.tsx
@@ -0,0 +1,30 @@
+"use client"
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+
+const GIFs = [
+  'webdev.gif',
+  'mobile.gif',
+  'blockchain.gif',
+  'tutor.gif'
+];
+
+const GIFDisplay: React.FC = () => {
+  const [currentGIFIndex, setCurrentGIFIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentGIFIndex((prevIndex) => (prevIndex + 1) % GIFs.length);
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <Image src={`/gifs/${GIFs[currentGIFIndex]}`} alt={`GIF ${currentGIFIndex + 1}`} width={500} height={400} />
+    </div>
+  );
+};
+
+export default GIFDisplay;
